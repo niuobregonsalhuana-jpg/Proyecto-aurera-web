87854772756bed0cab128d520755f1d4819c62ec
Niurka Obregon <niuobregonsalhuana@crackthecode.la>
2025-09-09 23:11:16 -0500

    Crear formulario de inicio de sesión con HTML y validación en JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Parte 1: control del popup //
 const btnAbrirBienvenida = document.getElementById('btnAbrirBienvenida');
 const popupBienvenida = document.getElementById('popupBienvenida');
 
@@ -25,3 +26,47 @@ popupBienvenida.addEventListener('click', (e) => {
   }
 });
 
+// Parte 2: Validacion del formulario //
+const formulario = document.getElementById('formularioLogin');
+const correoInput = document.getElementById('correo');
+const contraseñaInput = document.getElementById('contraseña');
+
+formulario.addEventListener('submit', function (e) {
+ e.preventDefault();
+
+  const correo = correoInput.value.trim().toLowerCase();
+  const contraseña = contraseñaInput.value.trim();
+
+  if (!correo.endsWith('@gmail.com')) {
+    correoInput.setCustomValidity('Solo se permiten correos @gmail.com');
+    correoInput.reportValidity(); 
+    return;
+  } else {
+    correoInput.setCustomValidity(''); 
+  }
+
+  if (contraseña.length < 6) {
+    contraseñaInput.setCustomValidity('La contraseña debe tener al menos 6 caracteres.');
+    contraseñaInput.reportValidity();
+    return;
+  } else {
+    contraseñaInput.setCustomValidity('');
+  }
+
+  alert('Inicio de sesión exitoso!');
+});
+
+// Parte 3: Mostrar y ocultar contraseña //
+const togglePasswordBtn = document.getElementById('togglePassword');
+
+togglePasswordBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const tipoActual = contraseñaInput.getAttribute('type');
+  const esPassword = tipoActual === 'password';
+
+  contraseñaInput.setAttribute('type', esPassword ? 'text' : 'password');
+
+  this.classList.toggle('bi-eye-slash-fill');
+  this.classList.toggle('bi-eye-fill');
+});
